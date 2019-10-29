2c240bd1be7e4fc9c3785c90eba78f340f987624
Benjamin Repingon <benjamin.repingon@protonmail.com>
2019-10-29 18:22:20 +0100

    add: Sentry

--- app/src/main/java/co/revely/peertube/PeerTubeApp.kt
+++ app/src/main/java/co/revely/peertube/PeerTubeApp.kt
@@ -2,6 +2,8 @@ package co.revely.peertube
 
 import android.app.Application
 import co.revely.peertube.di.*
+import io.sentry.Sentry
+import io.sentry.android.AndroidSentryClientFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -18,6 +20,8 @@ class PeerTubeApp : Application()
 		super.onCreate()
 		if (BuildConfig.DEBUG)
 			Timber.plant(Timber.DebugTree())
+
+		Sentry.init("https://3ef84b44b9d241948419ab216cfa6b78@sentry.io/1801039",  AndroidSentryClientFactory(this))
 
 		startKoin {
 			androidContext(this@PeerTubeApp)
